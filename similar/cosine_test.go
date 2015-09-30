3ac4e00cc69f3d39991fde61f226c513634cf91a
Jaehyun Yeom <jae.yeom@gmail.com>
2015-09-30 03:48:35 +0000

    Add more cosine similarity test cases.

--- similar/cosine_test.go
+++ similar/cosine_test.go
@@ -3,9 +3,28 @@ package similar
 import "fmt"
 
 func ExampleCosine() {
-	fmt.Printf("%.3f", Cosine(
+	testCases := []struct {
+		a, b []string
+	}{{
+		[]string{"a", "brown", "fox"},
+		[]string{"a", "brown", "fox"},
+	}, {
 		[]string{"a", "brown", "fox"},
 		[]string{"the", "red", "fox"},
-	))
-	// Output: 0.333
+	}, {
+		[]string{"a", "brown", "fox"},
+		[]string{"a", "a", "brown", "fox"},
+	}, {
+		[]string{"a", "brown", "fox"},
+		[]string{"a", "b", "brown", "fox"},
+	}}
+	for _, testCase := range testCases {
+		a, b := testCase.a, testCase.b
+		fmt.Printf("%v, %v: %.3f\n", a, b, Cosine(a, b))
+	}
+	// Output:
+	// [a brown fox], [a brown fox]: 1.000
+	// [a brown fox], [the red fox]: 0.333
+	// [a brown fox], [a a brown fox]: 0.943
+	// [a brown fox], [a b brown fox]: 0.866
 }
